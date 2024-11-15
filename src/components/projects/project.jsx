f7ef784a68ced2a561b4d96ccc5693f4cb2b3c21
Madhav <mvaliyap@purdue.edu>
2024-11-15 00:46:22 -0500

    Deployment fixes

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,7 +6,7 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, title, description, linkText, link } = props;
+	const {title, description, linkText, link } = props;
 
 	return (
 		<React.Fragment>
