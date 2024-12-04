b6555c25b9c927ade9b60158a34b726c3e24625d
Madhav <mvaliyap@purdue.edu>
2024-12-04 15:59:02 -0500

    seo p1

--- src/data/seo.js
+++ src/data/seo.js
@@ -3,7 +3,7 @@ const SEO = [
 		page: "home",
 		description:
 			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
@@ -17,49 +17,49 @@ const SEO = [
 		page: "projects",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "education",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "browser",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "policy",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "resources",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 
 	{
 		page: "resourcescs",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["purduearc"],
+		keywords: ["Madhav Valiyaparambil", "Angela Yan", "Misinformation", "Disinformation", "Purdue", "Purdue University"],
 	},
 ];
 
